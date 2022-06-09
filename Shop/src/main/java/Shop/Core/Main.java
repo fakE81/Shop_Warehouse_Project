bab75aaf7d74c3fb9fa062ba8b0a66c809dffa51
Laurynas <laurynas.seikis@visma.com>
2022-06-09 16:39:13 +0300

    1.1

--- Shop/src/main/java/Shop/Core/Main.java
+++ Shop/src/main/java/Shop/Core/Main.java
@@ -10,5 +10,7 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
         System.out.println(Warehouse.getTools());
+        System.out.println("Pull Request");
+
     }
 }
