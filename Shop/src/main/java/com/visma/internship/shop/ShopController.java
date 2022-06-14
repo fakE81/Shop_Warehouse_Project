4c017cc72ba8773f6cd9f8ef6fe2a06cd631c666
Laurynas <laurynas.seikis@visma.com>
2022-06-14 16:16:27 +0300

    Pom changes

--- Shop/src/main/java/com/visma/internship/shop/ShopController.java
+++ Shop/src/main/java/com/visma/internship/shop/ShopController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api")
 @RestController
 public class ShopController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String helloWorld(){
-        return "Hello World!";
+        return "Shop";
     }
 }
--- Warehouse/src/main/java/com/visma/internship/warehouse/WarehouseController.java
+++ Warehouse/src/main/java/com/visma/internship/warehouse/WarehouseController.java
@@ -2,13 +2,15 @@ package com.visma.internship.warehouse;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/")
 @RestController
-public class Warehouse {
-    @GetMapping("/")
+public class WarehouseController {
+    @GetMapping()
     public static String getTools(){
-        return "Hello World!!!";
+        return "Sveikisveiki!!";
     }
 
 }
