25cad0d1a3a984412ac959899977a855151ecbd8
Laurynas <laurynas.seikis@visma.com>
2022-06-21 08:10:26 +0300

    Started shop implementation

--- Shop/src/main/java/com/visma/internship/shop/Main.java
+++ Shop/src/main/java/com/visma/internship/shop/Main.java
@@ -4,17 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class Main extends SpringBootServletInitializer{
+public class Main{
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Main.class);
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 
 }
