db964cecf576459e660880bf9a1e173f391c3dc6
Laurynas <laurynas.seikis@visma.com>
2022-06-27 15:55:58 +0300

    Items list html file added

--- Shop/src/main/java/com/visma/internship/shop/controllers/ShopController.java
+++ Shop/src/main/java/com/visma/internship/shop/controllers/ShopController.java
@@ -4,15 +4,16 @@ import com.visma.internship.ItemDTO;
 import com.visma.internship.shop.services.WarehouseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/shop")
-@RestController
+@Controller
 public class ShopController {
 
     @Autowired
@@ -26,8 +27,9 @@ public class ShopController {
     }
 
     @RequestMapping("/items")
-    public List<ItemDTO> getItems(){
-        return warehouseService.getItems();
+    public String listItems(Model model){
+        model.addAttribute("items",warehouseService.getItems());
+        return "home";
     }
 
     @RequestMapping("/item/{id}")
